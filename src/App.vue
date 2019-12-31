d3db38c3f493d9b7ecd9dd9f2d4e0433ae0b9c39
rodsystems <rodrigo_rangel08@yahoo.com.br>
2019-12-31 00:07:35 -0300

    segundo comit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="F1" src="./assets/iconF1.png">
+    <F1Screen msg="Bem vindo ao F1 App Vue"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import F1Screen from './components/F1Screen.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    F1Screen
   }
 }
 </script>
---
+++ src/components/F1Screen.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="corpo">
+    <h1 class="centralizado">{{ msg }}</h1>
+    <h2>{{ titulo }}</h2>
+    <input
+      type="search"
+      v-on:input="filtro = $event.target.value"
+      class="filtro"
+      placeholder="filtre pelo piloto"
+    />
+    {{filtro}}
+    <ul class="lista-pilotos">
+      <li v-for="pilotos of pilotoComFiltro" v-bind:key="pilotos.driverId" class="lista-pilotos-item">
+        <meu-painel :titulo="foto.first_name">
+          <label class="imagem-responsiva" :text="pilotos.driverId"></label>
+        </meu-painel>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import Lista from "./shared/lista/Lista";
+export default {
+  components: {
+    "meu-painel": Lista
+  },
+  name: "F1Screen",
+  computed: {
+    pilotoComFiltro() {
+      if (this.filtro) {
+        // filtra a lista, por enquanto vamos retornar uma lista em branco
+        let exp = new RegExp(this.filtro.trim(), "i");
+        return this.pilotos.filter(pilotos => exp.test(pilotos.driverId));
+      } else {
+        // se o campo estiver vazio, não filtramos, retornamos a lista
+        return this.pilotos;
+      }
+    }
+  },
+  created() {
+    axios
+      .get("https://ergast.com/api/f1/2019/drivers.json")
+      .then(response => {
+        this.pilotos = response.MRData.DriverTable.Drivers;
+      })
+      .catch(e => {
+        this.erros.push(e);
+      });
+  },
+  methods: {
+    reverseMessage() {
+      this.titulo = this.titulo
+        .split("")
+        .reverse()
+        .join("");
+    }
+  },
+  data() {
+    return {
+      filtro: "",
+      message: "Olá usuário",
+      erros: [],
+      pilotos: [],
+      titulo: "Filtre pelo piloto"
+    };
+  },
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<style scoped>
+.centralizado {
+  text-align: center;
+}
+.corpo {
+  font-family: Helvetica, Arial, sans-serif;
+  margin: 0 auto;
+  width: 96%;
+  color: gray;
+}
+.lista-pilotos {
+  list-style: none;
+}
+.lista-pilotos .lista-pilotos-item {
+  display: inline-block;
+}
+.filtro {
+  display: block;
+  width: 100%;
+}
+</style>
